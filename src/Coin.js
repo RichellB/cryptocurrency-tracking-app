41babfbb26a0c02de3a20eaf6f17b8cfe4c8555b
richell <richell.bonfield12@gmail.com>
2020-12-09 23:03:01 -0500

    Fetched Coin Data from Api

---
+++ src/Coin.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Coin.css';
+
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange
+}) => {
+  return (
+    <div className='coin-container'>
+      <div className='coin-row'>
+        <div className='coin'>
+          <img src={image} alt='crypto' />
+          <h1>{name}</h1>
+          <p className='coin-symbol'>{symbol}</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
