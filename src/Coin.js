f41025938d92b0bb49fdcb213324e82436e28a45
richell <richell.bonfield12@gmail.com>
2020-12-09 23:09:06 -0500

    Modified Coin Layout

--- src/Coin.js
+++ src/Coin.js
@@ -18,7 +18,20 @@ const Coin = ({
           <h1>{name}</h1>
           <p className='coin-symbol'>{symbol}</p>
         </div>
-        
+        <div className='coin-data'>
+          <p className='coin-price'>${price}</p>
+          <p className='coin-volume'>${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className='coin-marketcap'>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
       </div>
     </div>
   );
